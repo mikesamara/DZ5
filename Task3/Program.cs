5124662bc3b1c6604fb4d66feef174479b6a9a70
Maik <samara.maik@yandex.by>
2023-02-06 17:07:04 +0300

    add task3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -2,12 +2,12 @@
 //Найдите разницу между максимальным и минимальным элементов массива.
 Console.WriteLine("Введите размер массива");
 int size= Convert.ToInt32(Console.ReadLine()); //задаем размер массивва
-int [] arr = new int [size]; // массив
+double[] arr = new double[size]; // массив
 System.Console.WriteLine("Массив");
 RandomArray (arr);
-int max = arr[1];
-int min = arr[1];
-int dif = 0;
+double max = arr[1];
+double min = arr[1];
+double dif = 0;
 for (int j = 0; j < size; j++)
 {
     if (arr[j] > max)
@@ -27,11 +27,11 @@ System.Console.WriteLine($"Минимальный элемент массива 
 System.Console.WriteLine();
 System.Console.WriteLine($"Разница между максимальным и минимальным элементом массива = " + dif);
 // функция заполнения массива
-void RandomArray(int[] arr)
+void RandomArray(double[] arr)
 {
     for (int i = 0; i < size; i++)
         {
-            arr[i] = new Random().Next(1,100);
+            arr[i] = Convert.ToDouble(new Random().Next(100,1000))/100;
             System.Console.Write(arr[i]+" ");
         }
 }
